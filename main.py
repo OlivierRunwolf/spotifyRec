b7b6220de958ae7dc33ab128abce81f9f17facb2
Guillaume-Lachapelle <92831445+Guillaume-Lachapelle@users.noreply.github.com>
2022-01-29 13:44:14 -0500

    relatedArtists

--- main.py
+++ main.py
@@ -18,4 +18,13 @@ def hello2():
     print(response)
     return "false"
 
+
+def listrelatedArtists():
+    my_headers = {'Authorization': 'BQDHOpu3aJsb9XrUjVIkd58e6SYYx5ip5mSMSFrGVagG-AalLsu5bd7PF8h0oXlfWwhTgRY6ZFHodPsuQsbxUNLTLtcf2C-Z_W14RWcJfgR7ifmJBlvQqLGhDaO6Y7hKlz2YXpiCKD746IyqcNJZ8zNwwU9ZMlbcvX0'}
+    response = requests.get("https://api.spotify.com/v1/artists/id/related-artists",headers=my_headers)
+    output = response.json()
+    print(response)
+    print(output)
+    return "false"
+
 run(app, host='localhost', port=8080)
