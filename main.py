20adc7918821c5601a92475e75ab8b8a640ea6dc
Olivier <olivier@renouf.com>
2022-01-29 12:13:16 -0500

    small views + correction

--- main.py
+++ main.py
@@ -1,25 +1,27 @@
-from bottle import Bottle, run
+from bottle import Bottle, run, static_file
 import requests
 
 app = Bottle()
 
 @app.route('/spotify')
 def hello():
-
     print("....")
+    return "Hello dsada!!!!"
 
 
-    return "Hello SPOTI!!!!"
-
+@app.route('/<filename>')
+def main(filename):
+    return static_file(filename, root='view/main')
 
 
 def hello2():
     response = requests.get("http://api.open-notify.org/astros.json")
     print(response)
+    # update -> commit -> push
     return "false"
 
-def listArtistUser (STring userId):
+def listArtistUser (userId):
     response = requests.get("http://api.open-notify.org/astros.json")
+    return True
 
-
-run(app, host='localhost', port=8080)
+run(app, host='localhost', port=8080, reloader=True)
