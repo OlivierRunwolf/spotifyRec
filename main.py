c0dbe0412773f890bc6be4e91a56c85718404c07
Amine Halimi <35849317+kilner99@users.noreply.github.com>
2022-01-29 17:08:28 -0500

    Adding skeleton of code

--- main.py
+++ main.py
@@ -36,6 +36,15 @@ def listArtistUser(user):
     #....
     return template('search', username=user['display_name'])
 
+def generateListRecommendedTracks():
+    recommendedTracks = [1]
+
+    return recommendedTracks
+#
+def listUnlistenedTracksFromUserTopArtists(user_id):
+
+def listTracksFromRelatedArtists(artist_id):
+
 @app.route('/related-artists')
 def listrelatedArtists(artist_id):
     # my_headers = {'Authorization': 'BQDHOpu3aJsb9XrUjVIkd58e6SYYx5ip5mSMSFrGVagG-AalLsu5bd7PF8h0oXlfWwhTgRY6ZFHodPsuQsbxUNLTLtcf2C-Z_W14RWcJfgR7ifmJBlvQqLGhDaO6Y7hKlz2YXpiCKD746IyqcNJZ8zNwwU9ZMlbcvX0'}
@@ -56,9 +65,18 @@ def listrelatedArtists(artist_id):
     rel_artists = results['artists']
 
     for rel_art in rel_artists:
+        # top track file for each artists
         rel_art_URI = 'spotify:artist:' + rel_art['id']
         topTracksFile = spotify.artist_top_tracks(rel_art_URI)
         topTracks = topTracksFile['tracks']
+
         print(rel_art['name'])
+        sizeTopTracks = len(topTracks)
+        # track1Number = random.randint(0,sizeTopTracks-2)
+        # track2Number = random.randint(0,sizeTopTracks-2)
+        # track3Number = random.randint(0,sizeTopTracks-2)
+        # print(topTracks[track1Number]['name'], ",", topTracks[track2Number]['name'], ",", topTracks[track3Number]['name'], "\n")
+        randomTracks = random.sample(range(0, sizeTopTracks), 3)
+        print(topTracks[randomTracks[0]]['name'], ";", topTracks[randomTracks[1]]['name'], ";", topTracks[randomTracks[2]]['name'], "\n")
 
 run(app, host='localhost', port=8080,reloader=True)
