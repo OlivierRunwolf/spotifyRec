e65c406bf1cfbb97d015c58e7b0db3d9643f67d0
Guillaume-Lachapelle <92831445+Guillaume-Lachapelle@users.noreply.github.com>
2022-01-29 14:06:54 -0500

    related-artists route

--- main.py
+++ main.py
@@ -26,13 +26,16 @@ def listArtistUser (userId):
     response = requests.get("http://api.open-notify.org/astros.json")
     return True
 
-
+@app.route('/related-artists')
 def listrelatedArtists():
     my_headers = {'Authorization': 'BQDHOpu3aJsb9XrUjVIkd58e6SYYx5ip5mSMSFrGVagG-AalLsu5bd7PF8h0oXlfWwhTgRY6ZFHodPsuQsbxUNLTLtcf2C-Z_W14RWcJfgR7ifmJBlvQqLGhDaO6Y7hKlz2YXpiCKD746IyqcNJZ8zNwwU9ZMlbcvX0'}
     response = requests.get("https://api.spotify.com/v1/artists/id/related-artists",headers=my_headers)
     output = response.json()
     print(response)
     print(output)
-    return "false"
+    if 'error' not in output:
+        return output
+    elif output['error']['status'] == 400 :
+        return "Error User Not found"
 
-run(app, host='localhost', port=8080)
+run(app, host='localhost', port=8080, reloader=True)
