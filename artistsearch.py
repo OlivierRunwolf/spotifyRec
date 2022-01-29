3643e572df89b868fd50c603cf57c7049ebf3ec0
Amine Halimi <35849317+kilner99@users.noreply.github.com>
2022-01-29 16:47:19 -0500

    Adding artistsearch file

--- artistsearch.py
+++ artistsearch.py
@@ -1,20 +1,36 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
-
+import random
 birdy_uri = 'spotify:artist:2WX2uTcsvV5OnS0inACecP'
 spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
 # results = spotify.user('Olivier')
 # print(results)
-results = spotify.artist_albums(birdy_uri, album_type='album')
-albums = results['items']
-while results['next']:
-    results = spotify.next(results)
-    albums.extend(results['items'])
-
-for album in albums:
-    print(album['name'])
+# results = spotify.artist_albums(birdy_uri, album_type='album')
+# albums = results['items']
+# while results['next']:
+#     results = spotify.next(results)
+#     albums.extend(results['items'])
+#
+# for album in albums:
+#     print(album['name'])
 results = spotify.artist_related_artists(birdy_uri)
-rel_artists = results['items']
-while results['next']:
-    results = spotify.next(results)
-    albums.extend(results['items'])
+rel_artists = results['artists']
+# while results['next']:
+#     results = spotify.next(results)
+#     rel_artists.extend(results['artists'])
+
+for rel_art in rel_artists:
+    #top track file for each artists
+    rel_art_URI = 'spotify:artist:' + rel_art['id']
+    topTracksFile = spotify.artist_top_tracks(rel_art_URI)
+    topTracks = topTracksFile['tracks']
+
+    print(rel_art['name'])
+    sizeTopTracks = len(topTracks)
+    # track1Number = random.randint(0,sizeTopTracks-2)
+    # track2Number = random.randint(0,sizeTopTracks-2)
+    # track3Number = random.randint(0,sizeTopTracks-2)
+    # print(topTracks[track1Number]['name'], ",", topTracks[track2Number]['name'], ",", topTracks[track3Number]['name'], "\n")
+    randomTracks = random.sample(range(0,sizeTopTracks),3)
+    print(topTracks[randomTracks[0]]['name'], ";", topTracks[randomTracks[1]]['name'], ";", topTracks[randomTracks[2]]['name'],"\n")
+
--- main.py
+++ main.py
@@ -36,15 +36,28 @@ def listArtistUser(user):
     return template('search', username=user['display_name'])
 
 @app.route('/related-artists')
-def listrelatedArtists():
-    my_headers = {'Authorization': 'BQDHOpu3aJsb9XrUjVIkd58e6SYYx5ip5mSMSFrGVagG-AalLsu5bd7PF8h0oXlfWwhTgRY6ZFHodPsuQsbxUNLTLtcf2C-Z_W14RWcJfgR7ifmJBlvQqLGhDaO6Y7hKlz2YXpiCKD746IyqcNJZ8zNwwU9ZMlbcvX0'}
-    response = requests.get("https://api.spotify.com/v1/artists/id/related-artists",headers=my_headers)
-    output = response.json()
-    print(response)
-    print(output)
-    if 'error' not in output:
-        return output
-    elif output['error']['status'] == 400 :
-        return "Error No related artists found found"
+def listrelatedArtists(artist_id):
+    # my_headers = {'Authorization': 'BQDHOpu3aJsb9XrUjVIkd58e6SYYx5ip5mSMSFrGVagG-AalLsu5bd7PF8h0oXlfWwhTgRY6ZFHodPsuQsbxUNLTLtcf2C-Z_W14RWcJfgR7ifmJBlvQqLGhDaO6Y7hKlz2YXpiCKD746IyqcNJZ8zNwwU9ZMlbcvX0'}
+    # response = requests.get("https://api.spotify.com/v1/artists/id/related-artists",headers=my_headers)
+    # output = response.json()
+    # print(response)
+    # print(output)
+    # if 'error' not in output:
+    #     return output
+    # elif output['error']['status'] == 400 :
+    #     return "Error No related artists found found"
+
+    #Access to
+    artist_uri = 'spotify:artist:' + artist_id
+    spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
+    #Gets the list of related artists
+    results = spotify.artist_related_artists(artist_uri)
+    rel_artists = results['artists']
+
+    for rel_art in rel_artists:
+        rel_art_URI = 'spotify:artist:' + rel_art['id']
+        topTracksFile = spotify.artist_top_tracks(rel_art_URI)
+        topTracks = topTracksFile['tracks']
+        print(rel_art['name'])
 
 run(app, host='localhost', port=8080,reloader=True)
