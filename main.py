cfc2d89db0aa858f269062c51e79dcd3fba1efe1
Olivier <olivier@renouf.com>
2022-01-29 10:40:03 -0500

    DO NOT CHANGE

--- main.py
+++ main.py
@@ -1,20 +1,9 @@
-from bottle import route, run
+from bottle import Bottle, run
 
-@route('/hello')
+app = Bottle()
+
+@app.route('/hello')
 def hello():
     return "Hello World!"
 
-
-from ensurepip import version
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('HEY GUYS ITS WURKINNN')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+run(app, host='localhost', port=8080)
